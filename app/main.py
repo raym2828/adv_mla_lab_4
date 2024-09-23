e1f211ac42f7d1a06388c73450c1bb0f18f3a086
raym2828 <raytechie@gmail.com>
2024-09-23 16:10:39 +1000

    update

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('ML Streamlit App')
+st.write("""
+## The Machine Learning App
+
+You can use this application in order to get predictions from a trained Adaboost model on the estimated number of shares for a news article.
+
+You will have to select the values that describe a news article.
+""")
+
+timedelta                = st.slider("timedelta", 0, 750, 340)
+n_tokens_title           = st.slider("n_tokens_title", 0, 25, 10)
